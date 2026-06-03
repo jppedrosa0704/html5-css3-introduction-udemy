b018fb177ffa4a4ebf79d1bc8677229013807bde
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-06-03 17:31:45 +0100

    Create lesson_1.py

---
+++ lesson-1/lesson_1.py
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Meu HTML</title>
+</head>
+<body>
+	<h1>Meu HTML</h1>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit.
+		Recusandae, itaque. Minus ex at odio consequatur autem nihil esse obcaecati
+		voluptates temporibus cum unde officiis enim, est vel iste tempore iusto.
+	</p>
+</body>
+</html>
